0017912a2e0a932e3dc05307473b4b2519198b80
Lisa Zapson <lisazapson@ymail.com>
2018-05-16 16:16:50 -0400

    added tests for app.jsx

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+
+// Use Jest Global describe method to group tests together,
+// two arg, description and a series of tests
+describe('App', () => {
+  const app = shallow(<App />)
+
+  it('initializes the `state` Logged In equal to false', () => {
+    expect(app.state().loggedIn).toEqual(false);
+  })
+
+})
--- src/components/Navbar/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Navbar from './Navbar';
-
-const navbar = shallow(<Navbar />);
-
-it('renders correctly', () => {
-    expect(navbar).toMatchSnapshot();
-})
