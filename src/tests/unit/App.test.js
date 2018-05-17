4369fcfeb078101261d1a33acfabb2a5d63ff5c5
Itamar Hassin <ihassin@mac.com>
2018-05-17 10:05:10 -0400

    Added lint

--- src/tests/unit/App.test.js
+++ src/tests/unit/App.test.js
@@ -5,10 +5,11 @@ import App from '../../App'
 // Use Jest Global describe method to group tests together,
 // two arg, description and a series of tests
 describe('App', () => {
-  const app = shallow(<App />)
+  const app = shallow(<App />);
 
   it('initializes the `state` Logged In equal to false', () => {
     expect(app.state().loggedIn).toEqual(false);
   })
 
-})
+});
+
