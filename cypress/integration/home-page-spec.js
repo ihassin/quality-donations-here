5f2ba127d9cad6dccd26f26007395da5537d0028
Itamar Hassin <ihassin@mac.com>
2018-05-17 20:10:06 -0400

    Pretty

--- cypress/integration/home-page-spec.js
+++ cypress/integration/home-page-spec.js
@@ -1,7 +1,6 @@
 describe("Home page", function () {
   it(".should() - assert that <title> is correct", function () {
     cy.visit("https://glacial-mountain-71826.herokuapp.com/");
-
     cy.title().should("include", "Quality Donations Here");
   });
 
